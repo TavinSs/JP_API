5b20821debe30e426039aebef678aaf408ffaba6
TavinSs <otaviomdie@gmail.com>
2023-09-17 17:21:27 -0300

    Add project files.

---
+++ JP_API/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace JP_API.Controllers
+{
+    [Route("api/´[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("ObterIdAluno")]
+        public async Task<IActionResult> ObterIdAluno()
+        {
+            try
+            {
+                Aluno aluno = new Aluno();
+                aluno.Id = 1;
+                return Ok(aluno);
+            }
+            catch (Exception) 
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
